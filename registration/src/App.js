e6132ff132e6e4456a162aeb780da7edd729421e
Bhosale Saurabh <saurabhbhosale8855@users.noreply.github.com>
2024-08-13 23:27:28 +0530

    first commit

---
+++ registration/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import EnrollmentForm from './Components/EnrollmentForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Saurabh</h1>
+    <EnrollmentForm />
+    </div>
+  );
+}
+
+export default App;
